46c080f6efd12aa7c5e4fb5d99013713cbf1ffad
ykyouhei <kyouhei.lab@gmail.com>
2017-11-17 14:32:54 +0900

    Initial Commit

---
+++ Clr2Assets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Clr2Assets
+//
+//  Created by 山口　恭兵 on 2017/11/17.
+//  Copyright © 2017年 Recruit Lifestyle Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
